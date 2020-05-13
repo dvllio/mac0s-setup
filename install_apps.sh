cf27133530e749db8de764b8e927b5a57bc2852f
dvllio <romain.deville@gmail.com>
2020-05-13 19:51:18 +0100

    add vscode to installer

--- install_apps.sh
+++ install_apps.sh
@@ -27,6 +27,7 @@ brew cask install iterm2
 brew cask install google-chrome
 brew cask install firefox
 brew cask install sublime-text
+brew cask install visual-studio-code
 brew cask install spotify
 brew cask install vlc
 brew cask install transmission
